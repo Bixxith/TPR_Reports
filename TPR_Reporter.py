4fd5b8efff23a372afd21a57986f52b97e2ca80b
Bixxith <39364622+Bixxith@users.noreply.github.com>
2022-01-18 23:11:49 -0500

    Program now catches Stray TPRs
    When the program compiles a report it will now automatically report any stray TPRs that might have been accidentally entered on the wrong date.

--- TPR_Reporter.py
+++ TPR_Reporter.py
@@ -9,25 +9,26 @@ from openpyxl import load_workbook, styles
 class TPR_Reporter:
     
     def __init__(self):
-        dateFormat = '%#m/%#d/%Y'
-        self.nextSaturday = self.getNextSaturday()
-        self.nextSaturdayDateFormatted = self.nextSaturday.strftime(dateFormat)
+        self.nextSaturday = self.getNextSaturday(pendulum.now())
+        self.nextThreeSaturdays = self.getNextThreeSaturdays()
+        self.nextSaturdayDateFormatted = self.formatDates(self.nextSaturday)
         self.brdFile = os.path.join(os.path.expanduser("~/Desktop"),
                                     "BRdata_Prices.xlsx")
 
-    # Setter for the self.nextSaturday variable.
-    def getNextSaturday(self):
-        return pendulum.now().next(pendulum.SATURDAY)
+    def formatDates(self, date):
+        dateFormat = '%#m/%#d/%Y'
+        return date.strftime(dateFormat)
     
-    # Checks to see if the BRD file has been updated to ensure accuracy
+    def getNextSaturday(self, date):
+        return date.next(pendulum.SATURDAY)
+    
     def checkUpdated(self):
         fileModifiedDate = date.fromtimestamp(os.path.getmtime(self.brdFile))
         return bool(fileModifiedDate == date.today())
     
-    # Gets the relevant data from the from the BRData file.
     def getData(self):
         tprColumn = 'TPR\nPrior'
-        dateColumnName = 'TPR To'
+        self.dateColumnName = 'TPR To'
         tprPriority = 99
         columnsToUse = "B,C,D,F,G,H,I,J,K,T"
         columnRenaming = {'Description': 'Item Description',
@@ -36,18 +37,27 @@ class TPR_Reporter:
                                'TPR\nPM': '  ',
                                'TPR\nPrice': 'TPR Price'}
         rawFile = pd.read_excel(self.brdFile, usecols=columnsToUse)
-        dateFilter = rawFile[rawFile[dateColumnName] == self.nextSaturdayDateFormatted]
-        tprFilter = dateFilter[dateFilter[tprColumn] == tprPriority]
+        filterOld = rawFile[rawFile[self.dateColumnName] >= self.nextSaturdayDateFormatted]
+        filterFirstSat = filterOld[filterOld[self.dateColumnName] != self.nextThreeSaturdays[0]]
+        filterSecondSat = filterFirstSat[filterFirstSat[self.dateColumnName] != self.nextThreeSaturdays[1]]
+        filterThirdSat = filterSecondSat[filterSecondSat[self.dateColumnName] != self.nextThreeSaturdays[2]]
+        tprFilter = filterThirdSat[filterThirdSat[tprColumn] == tprPriority]
         finalDataFile = tprFilter
         finalDataFile.rename(columns=columnRenaming,
                                         inplace=True)
         self.dataFile = finalDataFile
-    
-    # Setup for the outputted report and its directory          
+
+    def getNextThreeSaturdays(self):
+        saturdays = []
+        nextSat = self.nextSaturday
+        for i in range(1,4):
+            nextSat = self.getNextSaturday(nextSat)
+            saturdays.append(self.formatDates(nextSat))
+        return(saturdays)
+                
     def setupFiles(self):
         fileDateFormat ='%#m-%#d-%Y'
         nxtSatDate = self.nextSaturday.strftime(fileDateFormat)
-        
         self.reportFolder = os.path.expanduser("~\Desktop\TPR Report")
         if not os.path.exists(self.reportFolder):
             os.mkdir(self.reportFolder)
@@ -93,12 +103,10 @@ class TPR_Reporter:
         messagebox.showinfo(title="Report Compiled",
                             message=infoText)
 
-        
     def createSheets(self):
         upcString = '[<=99999]#;[<=9999999999]#####-#####;###-#####-#####'
         workbookFormats = {'upc':{'num_format': upcString},
                            'num':{'num_format': '$#.00'}}
-        dataFile = self.dataFile
         departments = {'Produce':[20,21,22,23,24,25,26,27,28,29,100],
                             'Meat':[30,31,32,33,34,35,36,37,38,39],
                             'Frozen':[40,41,42,43,44,45,46,47,48,49],
@@ -108,7 +116,8 @@ class TPR_Reporter:
                             'GM & HBC':[70,71,72,73,74,75,76,77,78,79,
                                         90,91,92,93,94,95,96,97,98,99],
                             'Grocery':[200,201,202,203,204,205,
-                                       206,207,208,209,210]}
+                                       206,207,208,209,210],
+                            'Stray TPRs':range(1,211)}
         columnsList = ["UPC",
                        "Item Description",
                        " ",
@@ -116,44 +125,60 @@ class TPR_Reporter:
                        "  ",
                        "TPR Price"]
         for dept in departments.keys():
-            numList = departments[dept]
-            departmentTPRs = dataFile[dataFile['Dept'].isin(numList)]
-            if not departmentTPRs.empty:
-                sortedTPRs = departmentTPRs.sort_values(by=['UPC'])
-                rows = sortedTPRs.index
-                sortedTPRs.to_excel(self.reportWriter, 
-                            sheet_name=dept,
-                            index=False,
-                            columns=columnsList)
-                reportWorkbook = self.reportWriter.book
-                moneyFormat = reportWorkbook.add_format(workbookFormats['num'])
-                upcFormat = reportWorkbook.add_format(workbookFormats['upc'])
-                reportWorksheet = self.reportWriter.sheets[dept]
+            if dept != 'Stray TPRs':
+                self.processDepts(dept, departments,columnsList, workbookFormats)
+            else:
+                self.processDepts(dept, departments,columnsList, workbookFormats, strays=True)
+                
+    def processDepts(self, dept, departments,columnsList, workbookFormats, strays=False):
+        regularTPRs = self.dataFile[self.dataFile[self.dateColumnName] == self.nextSaturdayDateFormatted]
+        print(regularTPRs)
+        strayTPRS = self.dataFile[self.dataFile[self.dateColumnName] != self.nextSaturdayDateFormatted]
+        print(strayTPRS)
+        numList = departments[dept]
+        if not strays:
+            departmentTPRs = regularTPRs[regularTPRs['Dept'].isin(numList)]
+        else:
+            departmentTPRs = strayTPRS[strayTPRS['Dept'].isin(numList)]
+        if not departmentTPRs.empty:
+            sortedTPRs = departmentTPRs.sort_values(by=['UPC'])
+            rows = sortedTPRs.index
+            sortedTPRs.to_excel(self.reportWriter, 
+                        sheet_name=dept,
+                        index=False,
+                        columns=columnsList)
+            reportWorkbook = self.reportWriter.book
+            moneyFormat = reportWorkbook.add_format(workbookFormats['num'])
+            upcFormat = reportWorkbook.add_format(workbookFormats['upc'])
+            reportWorksheet = self.reportWriter.sheets[dept]
+            if dept != 'Stray TPRs':
                 headerFormat = (f'&C&20TPR Report  |'
                                 f'|  {dept} Department  |'
                                 f'|  {self.nextSaturdayDateFormatted}')
-                reportWorksheet.set_header(headerFormat)
-                moneyFormat.set_align('center')
-                reportWorksheet.set_column('A:A', 14.86, upcFormat)
-                reportWorksheet.set_column('B:B', 38)
-                reportWorksheet.set_column('C:C', 2.29)
-                reportWorksheet.set_column('D:D', 11.86, moneyFormat)
-                reportWorksheet.set_column('E:E', 2.29)
-                reportWorksheet.set_column('F:F', 8.43, moneyFormat)
-    
-    # Parent function of the border processes
+            else:
+                headerFormat = (f'&C&20TPR Report  |'
+                                f'|  {dept} All Departments  |'
+                                f'|  {self.nextSaturdayDateFormatted}')
+            reportWorksheet.set_header(headerFormat)
+            moneyFormat.set_align('center')
+            reportWorksheet.set_column('A:A', 14.86, upcFormat)
+            reportWorksheet.set_column('B:B', 38)
+            reportWorksheet.set_column('C:C', 2.29)
+            reportWorksheet.set_column('D:D', 11.86, moneyFormat)
+            reportWorksheet.set_column('E:E', 2.29)
+            reportWorksheet.set_column('F:F', 8.43, moneyFormat)
+            
     def addBorders(self):
         self.processSheets()
-    
-    # Loads the worksheets and passes each sheet to get borders added            
+           
     def processSheets(self):
         workbook = load_workbook(self.reportFile)
         for sheets in workbook.sheetnames:
             sheet = workbook[sheets]
             self.setBorder(sheet)
-        workbook.save(self.reportFile)
-    
-    # Adds bottom borders to the selected worksheets    
+        workbook.save(self.reportFile )
+        workbook.close()
+      
     def setBorder(self, worksheet):
         workSheetRange = ['A', 'F']
         thinFormat = styles.Side(border_style="dotted", color="000000")
@@ -164,9 +189,9 @@ class TPR_Reporter:
                 if cell.row % 2 != 0 and cell.row != 1:
                     cell.border = addBorder
 
-    # Function for manipulating the resulting file after creation.
     def postProcessing(self):
         self.addBorders()
+     
         
 class TPR_Reporter_GUI:
     
